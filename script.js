b65c81a65b7b4fe34602a2c3c6792f644205d956
Claudio <claudiovarrosi@gmail.com>
2022-05-24 16:25:13 +0200

    Lavoro completo

---
+++ script.js
@@ -0,0 +1,8 @@
+var container = new Vue({
+    el: "#container",
+    data: {
+        message : "Salve Salvino",
+        image : "https://jamara-shop.com/media/image/product/2150/md/410110_rc-water-animals-papera-24ghz~5.jpg",
+        imgAlt : "image"
+    },
+})
